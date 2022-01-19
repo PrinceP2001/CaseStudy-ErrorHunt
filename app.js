654d694adab78510f7b0d07d8413c145506db0d3
PRINCE P <91449814+PrinceP2001@users.noreply.github.com>
2022-01-19 20:09:01 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require ('path'); 
 const cors = require('cors');
 const bodyParser=require("body-parser");
-var port=process.env.PORT || 5000;
+var port=5000;
 
 const nav= [
     {
